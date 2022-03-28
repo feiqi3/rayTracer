97cc5e6da39e135c9b95f3118ccaa460d5247a0a
feiqi3 <zjgtzn@outlook.com>
2022-03-28 16:44:11 +0800

    add debug macro

---
+++ include/Macro.h
@@ -0,0 +1,15 @@
+/*
+ * @Author: feiqi3
+ * @Date: 2022-03-28 16:37:42
+ * @LastEditTime: 2022-03-28 16:37:42
+ * @LastEditors: feiqi3
+ * @Description: |---description here---|
+ * @FilePath: \rayTracer\include\Macro.h
+ * ->blog: feiqi3.cn <-
+ */
+#ifndef MACRO_H_
+#define MACRO_H_
+
+#define DEBUG
+
+#endif
