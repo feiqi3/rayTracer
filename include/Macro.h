a390d1b398f787664a7dd7fdecce8a07de4f790e
feiqi3 <zjgtzn@outlook.com>
2022-05-16 09:36:15 +0800

    add some log macro

--- include/Macro.h
+++ include/Macro.h
@@ -1,15 +1,29 @@
 /*
  * @Author: feiqi3
  * @Date: 2022-03-28 16:37:42
- * @LastEditTime: 2022-05-10 14:01:35
+ * @LastEditTime: 2022-05-15 12:01:29
  * @LastEditors: feiqi3
  * @Description: |Some macros here|
- * @FilePath: \raytracer_post\rayTracer\include\Macro.h
+ * @FilePath: \rayTracer\include\Macro.h
  * ->blog: feiqi3.cn <-
  */
 #ifndef MACRO_H_
 #define MACRO_H_
-
+#include <string>
+#include<regex>
 //#define DEBUG
-#define BIT(x) 1<<(x)
+#define BIT(x) 1 << (x)
+
+#define GET_CLASS_NAME(cls)                                                    \
+  const std::string clsname() const { return std::string(#cls); };
+
+#define CLASS_INFO                                                             \
+  virtual const std::string __className() const{                                          \
+    std::string nameBuffer(__FUNCTION__);                                      \
+    std::smatch result;                                                        \
+    std::regex pattern("(\\w+)::");                                            \
+    std::regex_search(nameBuffer, result, pattern);                            \
+    return result[1];                                                          \
+  }
+
 #endif
