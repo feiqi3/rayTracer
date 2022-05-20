01a4eabeb6873de419ada5afb9e3ff548b78bcc5
feiqi3 <zjgtzn@outlook.com>
2022-05-20 23:24:53 +0800

    A simple Matrix.

---
+++ include/math/matrix.h
@@ -0,0 +1,116 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "vector.h"
+#include "vector4.h"
+#include <string>
+
+class mat4 {
+public:
+  vec4 cmp[4];
+  mat4(vec4 a, vec4 b, vec4 c, vec4 d) {
+    cmp[0] = a;
+    cmp[1] = b;
+    cmp[2] = c;
+    cmp[3] = d;
+  }
+
+  vec4 &operator[](int index) { return cmp[index]; }
+
+  static mat4 Identity();
+
+  std::string toString()
+  {
+      return "Mat4:\n"+cmp[0].toString()+"\n"+cmp[1].toString()+"\n"+cmp[2].toString()+"\n"+cmp[3].toString();
+  }
+};
+
+
+inline mat4 IdentityMat() {
+  return mat4(vec4(1, 0, 0, 0), vec4(0, 1, 0, 0), vec4(0, 0, 1, 0),
+              vec4(0, 0, 0, 1));
+}
+
+inline mat4 mat4::Identity() { return IdentityMat(); }
+
+inline vec4 __getCol(mat4 a, int i) {
+  return vec4(a.cmp[0].vec[i], a.cmp[1].vec[i], a.cmp[2].vec[i],
+              a.cmp[3].vec[i]);
+}
+
+inline double __cmp_wise_mul_then_add(const vec4 a, const vec4 &b) {
+  vec4 tmp = a * b;
+  return tmp.x() + tmp.y() + tmp.z() + tmp.w();
+}
+
+// Matrix4 dot
+inline mat4 operator*(const mat4 &a, const mat4 &b) {
+  double m00 = __cmp_wise_mul_then_add(a.cmp[0], __getCol(b, 0));
+  double m01 = __cmp_wise_mul_then_add(a.cmp[0], __getCol(b, 1));
+  double m02 = __cmp_wise_mul_then_add(a.cmp[0], __getCol(b, 2));
+  double m03 = __cmp_wise_mul_then_add(a.cmp[0], __getCol(b, 3));
+  double m10 = __cmp_wise_mul_then_add(a.cmp[1], __getCol(b, 0));
+  double m11 = __cmp_wise_mul_then_add(a.cmp[1], __getCol(b, 1));
+  double m12 = __cmp_wise_mul_then_add(a.cmp[1], __getCol(b, 2));
+  double m13 = __cmp_wise_mul_then_add(a.cmp[1], __getCol(b, 3));
+  double m20 = __cmp_wise_mul_then_add(a.cmp[2], __getCol(b, 0));
+  double m21 = __cmp_wise_mul_then_add(a.cmp[2], __getCol(b, 1));
+  double m22 = __cmp_wise_mul_then_add(a.cmp[2], __getCol(b, 2));
+  double m23 = __cmp_wise_mul_then_add(a.cmp[2], __getCol(b, 3));
+  double m30 = __cmp_wise_mul_then_add(a.cmp[3], __getCol(b, 0));
+  double m31 = __cmp_wise_mul_then_add(a.cmp[3], __getCol(b, 1));
+  double m32 = __cmp_wise_mul_then_add(a.cmp[3], __getCol(b, 2));
+  double m33 = __cmp_wise_mul_then_add(a.cmp[3], __getCol(b, 3));
+  // Fuck me
+  return mat4(vec4(m00, m01, m02, m03), vec4(m10, m11, m12, m13),
+              vec4(m20, m21, m22, m23), vec4(m30, m31, m32, m33));
+}
+
+inline vec4 operator*(const mat4 &mat, const vec4 &vec) {
+  double a = __cmp_wise_mul_then_add(mat.cmp[0], vec);
+  double b = __cmp_wise_mul_then_add(mat.cmp[1], vec);
+  double c = __cmp_wise_mul_then_add(mat.cmp[2], vec);
+  double d = __cmp_wise_mul_then_add(mat.cmp[3], vec);
+  return vec4(a, b, c, d);
+}
+
+inline mat4 getTranslate(const vec3 &vec) {
+  mat4 tmp = IdentityMat();
+  tmp.cmp[0].vec[3] = vec[0];
+  tmp.cmp[1].vec[3] = vec[1];
+  tmp.cmp[2].vec[3] = vec[2];
+  return tmp;
+}
+
+inline mat4 getScale(const vec3 &vec) {
+  mat4 tmp = IdentityMat();
+  tmp.cmp[0].vec[0] = vec[0];
+  tmp.cmp[1].vec[1] = vec[1];
+  tmp.cmp[2].vec[2] = vec[2];
+  return tmp;
+}
+
+// Rodrigues's rotation formula
+inline mat4 getRotate(double theta, const vec3 &v) {
+  double a = 1 - cos(theta);
+  double s = sin(theta);
+  double c = cos(theta);
+  double m00 = a * v.x() * v.x() + c;
+  double m01 = a * v.x() * v.y() - s * v.z();
+  double m02 = a * v.x() * v.z() + s * v.y();
+  double m03 = 0;
+  double m10 = a * v.y() * v.x() + s * v.z();
+  double m11 = a * v.y() * v.y() + c;
+  double m12 = a * v.y() * v.z() - s * v.x();
+  double m13 = 0;
+  double m20 = a * v.z() * v.x() - s * v.y();
+  double m21 = a * v.z() * v.y() + s * v.x();
+  double m22 = a * v.z() * v.z() + c;
+  double m23 = 0;
+
+  // Kill me
+  return mat4(vec4(m00, m01, m02, m03), vec4(m10, m11, m12, m13),
+              vec4(m20, m21, m22, m23), vec4(0, 0, 0, 1));
+}
+
+#endif
