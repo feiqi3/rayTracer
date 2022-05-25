4aaab3dbf690aa44975d7826f5baf2a94db1f405
feiqi3 <zjgtzn@outlook.com>
2022-05-25 12:06:41 +0800

    ..

---
+++ src/presets.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: feiqi3
+ * @Date: 2022-05-25 09:32:19
+ * @LastEditTime: 2022-05-25 12:06:40
+ * @LastEditors: feiqi3
+ * @Description: |---description here---|
+ * @FilePath: \rayTracer\src\presets.cpp
+ * ->blog: feiqi3.cn <-
+ */
+#include "object/scene.h"
+#include "hitableList.h"
+#include "renderPass/camera.h"
+#include "RenderQueue.h"
+
