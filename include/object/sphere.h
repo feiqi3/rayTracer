f35a33aef17f12b8d05e6e2d1e4bb351f34abd1d
feiqi3 <zjgtzn@outlook.com>
2022-08-26 10:59:43 +0800

    Implement hitable's function

--- include/object/sphere.h
+++ include/object/sphere.h
@@ -11,9 +11,11 @@
 #ifndef _SPHERE_H_
 #define _SPHERE_H_
 
+#include "aabb.h"
 #include "hitable.h"
 #include "math/vector.h"
 #include <memory>
+#include <utility>
 
 class sphere : public hitable {
 private:
@@ -22,7 +24,8 @@ private:
 
 public:
   sphere() {}
-  sphere(vec3 center, double r,std::shared_ptr<material> _material) : cen(center), radius(r),mat_ptr(_material) {}
+  sphere(vec3 center, double r, std::shared_ptr<material> _material)
+      : cen(center), radius(r), mat_ptr(_material) {}
 
   std::shared_ptr<material> mat_ptr;
   // setter and getter
@@ -32,37 +35,42 @@ public:
   void setRadius(const double r) { radius = r; }
 
   bool hit(const ray &r, double t_min, double t_max,
-           record &rec) const override {
-    vec3 o2o = r.orig - cen;
-    double a = dot(r.direction(), r.direction());
-    double half_b = dot(r.direction(), (o2o));
-    double c = dot(o2o, o2o) - radius * radius;
-    double dis = half_b * half_b - a * c;
-    if (dis < 0) {
+           record &rec) const override;
+
+  bool bounding_box(AABB &box_out) const override;
+};
+
+inline bool sphere::hit(const ray &r, double t_min, double t_max,
+                        record &rec) const {
+  vec3 o2o = r.orig - cen;
+  double a = dot(r.direction(), r.direction());
+  double half_b = dot(r.direction(), (o2o));
+  double c = dot(o2o, o2o) - radius * radius;
+  double dis = half_b * half_b - a * c;
+  if (dis < 0) {
+    return false;
+  }
+  double division_a = 1 / a;
+  double sqrt_dis = sqrt(dis);
+  double root = (-half_b - sqrt_dis) * division_a;
+  if (root > t_max || root < t_min) {
+    root = (-half_b - sqrt_dis) * division_a;
+    if (root > t_max || root < t_min) {
       return false;
     }
-    double division_a = 1 / a;
-    double sqrt_dis = sqrt(dis);
-    double root = (-half_b - sqrt_dis) * division_a;
-
-    if (root > t_max || root < t_min) {
-      root = (-half_b - sqrt_dis) * division_a;
-      if (root > t_max || root < t_min) {
-        return false;
-      }
-    }
-
-    rec.t = root;
-
-    rec.p = r.at(rec.t);
-
-    rec.mat_ptr = mat_ptr;
-
-    rec.normal = (rec.p - cen) / radius; // normalized
-    set_face_normal(r, rec.normal, rec);
-
-    return true;
   }
-};
+  rec.t = root;
+  rec.p = r.at(rec.t);
+  rec.mat_ptr = mat_ptr;
+  rec.normal = (rec.p - cen) / radius; // normalized
+  set_face_normal(r, rec.normal, rec);
+  return true;
+}
+
+inline bool sphere::bounding_box(AABB &box_out) const {
+  box_out = AABB(getCenter() - vec3(radius), getCenter() - vec3(radius));
+  return true;
+}
+
 
 #endif
