b7c62abc13861636ff2598f45e79623146521d68
feiqi3 <zjgtzn@outlook.com>
2022-08-26 23:09:37 +0800

    Add texture support to mat lambertian

--- include/material/lambertian.h
+++ include/material/lambertian.h
@@ -11,26 +11,30 @@
 #ifndef LAMBERTIAN_H_
 #define LAMBERTIAN_H_
 
-#include "material.h"
 #include "hitableList.h"
+#include "material.h"
 #include "math/vector.h"
 #include "ray.h"
+#include "texture/constant_color.h"
+#include "texture/texture.h"
+#include <memory>
+
 class lambertian : public material {
   virtual bool scatter(const ray &_in, const record &hit_rec,
-                       color &attenuation, ray &scattered) const override
-                       {
-                           vec3 scatter_dir = hit_rec.normal + rand_in_unit_sphere();
-                           if (scatter_dir.is_close_to_zero()) {
-                               scatter_dir = hit_rec.normal;
-                           }
-                           scattered = ray(hit_rec.p,scatter_dir);
-                           attenuation = albedo;
-                           return true;
-                       }
+                       color &attenuation, ray &scattered) const override {
+    vec3 scatter_dir = hit_rec.normal + rand_in_unit_sphere();
+    if (scatter_dir.is_close_to_zero()) {
+      scatter_dir = hit_rec.normal;
+    }
+    scattered = ray(hit_rec.p, scatter_dir);
+    attenuation = albedo->value(hit_rec.u, hit_rec.v, hit_rec.p);
+    return true;
+  }
 
-    public:lambertian(const color& _albedo):albedo(_albedo){}
-
-    public:color albedo;
-
+public:
+  lambertian(const std::shared_ptr<texture> &_albedo) : albedo(_albedo) {}
+  lambertian(const vec3& color) : albedo(std::make_shared<constant_color>(color)) {}
+public:
+  std::shared_ptr<texture> albedo;
 };
 #endif
