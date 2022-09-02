dba38dea913530a899830d6fffb8c5a32f3b3a56
feiqi3 <zjgtzn@outlook.com>
2022-09-02 09:19:40 +0800

    Add light support.

--- include/material/material.h
+++ include/material/material.h
@@ -18,6 +18,10 @@ struct record;
 class material {
 public:
 virtual bool scatter(const ray& _in,const record& hit_rec,color& out_attenuation,ray& out_scattered) const = 0;
+
+virtual vec3 emitted(double u,double v,const vec3& p)const{
+    return vec3(0);
+    }
 };
 
 #endif
