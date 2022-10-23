1eb35a7b53da9354ddc32a91630b49cbb079aff5
feiqi3 <zjgtzn@outlook.com>
2022-10-23 14:29:57 +0800

    Add some sample functions.

--- include/math/baiscSampleFunctions.h
+++ include/math/baiscSampleFunctions.h
@@ -2,6 +2,7 @@
 #define BASIC_SAMPLE_FUN_
 #include "vector.h"
 #include "tool/common.h"
+#include <algorithm>
 #include <cmath>
 #include <stdlib.h>
 
@@ -17,10 +18,10 @@ inline void ConcentricSampleDisk(float* x,float* y){
     float r,t;
     if(abs(a) > abs(b)){
         r = a;
-        t = b / a * inv_pi4;
+        t = b / a * inv_4pi;
     }else {
         r = b;
-        t = inv_pi2 -a / b *  inv_pi4;
+        t = inv_2pi -a / b *  inv_4pi;
     }
     *x = r * cos(t);
     *y = r * sin(t);
@@ -29,8 +30,8 @@ inline void ConcentricSampleDisk(float* x,float* y){
 
 inline vec3 sampleCosWeighted(){
     float x,y;
-    ConcentricSampleDisk(&x, &y);;
-    float z = sqrt(fmax(0.,1 - x * x - y * y));
+    uniformSampleDisk(&x, &y);;
+    float z = sqrt(std::max(0.0001f,1 - x * x - y * y));
     return vec3(x,z,y);
 }
 
